8c02ce24458382669ffa73d059b120aef6829e89
Torgeir Kruke <torgeir.kruke@gmail.com>
2023-08-30 19:17:35 +0200

    Python PoC v1

--- Python/main.py
+++ Python/main.py
@@ -1,8 +1,11 @@
 import tkinter as tk
 from tkinter import ttk
+from tkinter import messagebox
 import mysql.connector
 from firebase import firebase
 import pyrebase
+
+
 # Initialize Tkinter GUI
 root = tk.Tk()
 root.title("KrUltra Database Manager")
@@ -44,39 +47,159 @@ def connect_to_mariadb():
         print(f"Failed to connect to MariaDB: {e}")
         return None
 
-# Function to fetch data from MariaDB
-def fetch_data_from_mariadb():
+# Function to handle "Exit"
+def exit_app():
+    root.destroy()
+
+# Function to handle "Upload New event Data"
+def upload_new_event_data():
+    response = messagebox.askyesno("Advarsel", "NB: ALLE EKSISTERENDE DATA SLETTES! Vil du fortsette?")
+    if response:
+        fetch_and_display_active_events()
+
+# Function to fetch and display active events
+def fetch_and_display_active_events():
     mydb = connect_to_mariadb()
     if mydb:
         mycursor = mydb.cursor()
-        mycursor.execute("SELECT * FROM rfid_reader")
-        result = mycursor.fetchall()
-        for row in result:
-            print(row)
+        sql_query = "SELECT id, long_name, year, edition FROM event WHERE status=1"
+        mycursor.execute(sql_query)
+        active_events = mycursor.fetchall()
+
+        if active_events:
+            # Display events for the user to select, we will implement this function next
+            display_event_selection(active_events)
+        else:
+            messagebox.showinfo("Ingen aktive løp", "Det finnes ingen aktive løp i databasen.")
 
 
-def fetch_data_from_firebase():
+# Function to display active events for user selection
+def display_event_selection(active_events):
+    event_window = tk.Toplevel(root)
+    event_window.title("Velg et aktivt løp")
+    
+    tk.Label(event_window, text="Velg et løp fra listen:").pack()
+    
+    event_var = tk.StringVar()
+    event_var.set("Velg et løp")
+    
+    event_menu = ttk.Combobox(event_window, textvariable=event_var, values=[event[1] for event in active_events])
+    event_menu.pack()
+    
+    def select_event():
+        selected_event = event_var.get()
+        if selected_event != "Velg et løp":
+            selected_event_id = next((event[0] for event in active_events if event[1] == selected_event), None)
+            if selected_event_id is not None:
+                # Proceed to Step 2 to upload data to Firebase
+                upload_event_data_to_firebase(selected_event_id, event_window)
+            event_window.destroy()
+
+    tk.Button(event_window, text="Velg", command=select_event).pack()
+
+
+# Function to upload runners data to Firebase
+def upload_runners_data_to_firebase(event_id):
     try:
-        data = db.child("registrations").get(token=user_id_token).val()
-        print(data)
+        mydb = connect_to_mariadb()
+        if mydb:
+            mycursor = mydb.cursor()
+            query = """
+            SELECT 
+                race.name AS race_name,
+                person.first_name AS participant_first_name,
+                person.last_name AS participant_last_name,
+                participant.bib AS participant_bib,
+                rfid_tag.bib AS rfid_bib,
+                rfid_tag.uid AS rfid_uid
+            FROM race
+            JOIN participant ON race.id = participant.race_id
+            JOIN person ON participant.person_id = person.id
+            JOIN participant_tag ON participant.id = participant_tag.participant_id
+            JOIN rfid_tag ON participant_tag.rfid_tag_id = rfid_tag.id
+            WHERE race.event_id = %s AND race.status = 1
+            AND participant.status = 1 AND rfid_tag.status = 1;
+            """
+            mycursor.execute(query, (event_id,))
+            rows = mycursor.fetchall()
+            
+            # Create a list of dictionaries to hold the runner data
+            runner_data = {}
+            for row in rows:
+                rfid_uid = row[5]
+                runner_data[rfid_uid] = {
+                    'race_name': row[0],
+                    'participant_first_name': row[1],
+                    'participant_last_name': row[2],
+                    'participant_bib': row[3],  # This should be the same as 'rfid_bib'
+                    'rfid_bib': row[4],
+                }
+              
+            # Upload to Firebase
+            db.child("runners").set(runner_data, token=user_id_token)
+            
+            print("Successfully uploaded runners data to Firebase.")
+            
     except Exception as e:
-        print(f"Failed to fetch data from Firebase: {e}")
+        print(f"Failed to upload runners data to Firebase: {e}")
         return None
 
 
-# Function to upload data to Firebase
-def upload_to_firebase():
-    try:
-        db.child("testdata").push({"name": "John Doe"}, user['idToken'])
-    except Exception as e:
-        print(f"Failed to upload data to Firebase: {e}")
-        return None
+# Function to upload event data to Firebase
+def upload_event_data_to_firebase(selected_event_id, event_window):
+    # Step 2.1: Fetch data from MariaDB
+    mydb = connect_to_mariadb()
+    if mydb:
+        mycursor = mydb.cursor()
+        sql_query = """
+        SELECT rfid_reader.checkpoint_id, rfid_reader.chip_id, rfid_reader.description, event.id, rfid_reader.name 
+        FROM event 
+        JOIN checkpoint ON event.id = checkpoint.event_id 
+        JOIN rfid_reader ON checkpoint.id = rfid_reader.checkpoint_id 
+        WHERE event.id = %s
+        """
+        mycursor.execute(sql_query, (selected_event_id,))
+        rfid_readers = mycursor.fetchall()
         
+        # Step 2.2: Delete existing data in Firebase
+        db.child("rfid_readers").remove(token=user_id_token)
+        
+        # Step 2.3: Upload new event data to Firebase
+        for reader in rfid_readers:
+            reader_data = {
+                "checkpoint_id": reader[0],
+                "chip_id": reader[1],
+                "description": reader[2],
+                "event_id": reader[3],
+                "name": reader[4]
+            }
+            db.child("rfid_readers").push(reader_data, token=user_id_token)
 
-# Add buttons to trigger functions
-ttk.Button(root, text="Connect to MariaDB", command=connect_to_mariadb).pack(pady=20)
-ttk.Button(root, text="Fetch Data from MariaDB", command=fetch_data_from_mariadb).pack(pady=20)
-ttk.Button(root, text="Upload to Firebase", command=upload_to_firebase).pack(pady=20)
-ttk.Button(root, text="Fetch Data from Firebase", command=fetch_data_from_firebase).pack(pady=20)
+        # Step 2.4: Upload runners data to Firebase
+        upload_runners_data_to_firebase(selected_event_id)
+        
+        print("Successfully uploaded data to Firebase")
+        messagebox.showinfo("Suksess", "Dataene ble lastet opp til Firebase.")
+        event_window.destroy()
+
+        # Spør brukeren om å slette eksisterende registreringer
+        delete_registrations = messagebox.askyesno("Slett registreringer", "Ønsker du også å slette alle tidligere registreringer?")
+        if delete_registrations:
+            try:
+                db.child("registrations").remove(token=user_id_token)  # Slett alle data under "registrations"
+                messagebox.showinfo("Suksess", "Alle registreringer ble fjernet.")
+            except Exception as e:
+                messagebox.showerror("Feil", f"Klarte ikke å slette registreringer: {e}")
+    else:
+        messagebox.showerror("Feil", "Klarte ikke å koble til MariaDB.")
+        
+# Create buttons for the main menu
+upload_button = ttk.Button(root, text="Last opp data for nytt løp", command=upload_new_event_data)
+exit_button = ttk.Button(root, text="Avslutt", command=exit_app)
+
+# Place the buttons on the window
+upload_button.grid(row=0, column=0, padx=10, pady=10)
+exit_button.grid(row=1, column=0, padx=10, pady=10)
+
 
 root.mainloop()
